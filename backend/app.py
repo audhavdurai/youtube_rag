b38d9eb9055df093a094f8b8045be451e0302d6f
Audhav Durai <audhavdurai@audhavs-air.myfiosgateway.com>
2025-01-04 12:09:44 -0500

    fix port

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,7 @@ from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 import clip
 from datetime import datetime
 import torch
+import os
 import uuid
 
 
@@ -337,4 +338,6 @@ def update_chat(chat_id):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    # app.run(debug=True, host='127.0.0.1', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
